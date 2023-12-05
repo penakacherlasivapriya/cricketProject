680640cea6fcc6649e8846b9c9c66cf4d798f1d1
Priya <97936353+penakacherlasivapriya@users.noreply.github.com>
2023-12-05 16:21:37 +0530

    Add files via upload
    Here I am using Angular17 for frontend and Springboot for backend. In this project we can get to know the live status of cricket match and its score and also its previous score.

---
+++ CricketInformer/crickfront/src/app/pages/history/history.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ApiCallService } from '../../services/api-call.service';
+import { MatchCardComponent } from '../../match-card/match-card.component';
+
+@Component({
+  selector: 'app-history',
+  standalone: true,
+  imports: [CommonModule,MatchCardComponent],
+  templateUrl: './history.component.html',
+  styleUrl: './history.component.css'
+})
+export class HistoryComponent  implements OnInit{
+  allMatch: any;
+  constructor(private _api:ApiCallService){
+
+  }
+  ngOnInit(): void {
+    this._api.getAllMatches().subscribe(
+      {
+        next:data=>{
+          this.allMatch=data
+        },
+        error:error=>{
+          console.log(error)
+        }
+      }
+    )
+  }
+
+}
